e4463856359ab38312fa2fbf2bf01f2882379991
Sandesh <mainalisandesh8@gmail.com>
2024-05-10 17:13:55 +0545

    Clousre code

--- src/main/java/com/learn/dams/contollers/ClosureController.java
+++ src/main/java/com/learn/dams/contollers/ClosureController.java
@@ -2,6 +2,7 @@ package com.learn.dams.contollers;
 
 import com.learn.dams.dto.ClosureDto;
 import com.learn.dams.services.ClosureService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api")
 public class ClosureController {
+    @Autowired
     private ClosureService closureService;
 
     @PostMapping("/{conceptId}")
